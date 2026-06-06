80bc74c6de1554aca646d522fa54003894d47184
Chinmay Biswas <cool.dude5470@gmail.com>
2026-06-06 16:40:37 +0530

    doing some more videos

---
+++ all codes/day39-javascrip_starting/index.js
@@ -0,0 +1,3 @@
+console.log("hello yoooo jii...")
+console.log("hello jii...")
+// use node index.js in terminal to run
---
+++ all codes/day43/index.js
@@ -0,0 +1,21 @@
+console.log("chinmay");
+function name(){
+    console.log("chinamy biswas")
+}
+name();
+
+function countprint(i){
+    for(let a=0;a<i;a++){
+        console.log(a);
+    }
+}
+countprint(5);
+
+function sum(num1,num2){
+    let sum=num1+num2;
+    return sum;
+}
+let a=sum(5,6);
+console.log(a);
+
+ 
